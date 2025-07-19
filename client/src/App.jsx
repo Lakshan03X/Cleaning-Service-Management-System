7c748f6d223e356fca4cbc21b0de9ec3112baada
Lakshan03X <nipunlakshan9255@gmail.com>
2025-07-19 09:50:23 +0530

    initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import './index.css'
+
+function App() {
+
+  return (
+    <Router>
+      <Routes>
+        {/* Define your routes here */}
+      </Routes>
+    </Router>
+  )
+}
+
+export default App
